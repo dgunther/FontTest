d3c1f10c2755ee584fd1592f51b529875d75c468
David Gunther <david.gunther@softwaremotor.com>
2018-10-04 08:57:20 -0700

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.roundedButton1 = new WindowsFormsApplication1.RoundedButton();
+            this.rbutton3 = new WindowsFormsApplication1.RoundedButton();
+            this.rbutton2 = new WindowsFormsApplication1.RoundedButton();
+            this.rbutton1 = new WindowsFormsApplication1.RoundedButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(112, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 79);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Count";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 80);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Count";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // roundedButton1
+            // 
+            this.roundedButton1.Appearance.Font = new System.Drawing.Font("MS Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundedButton1.Appearance.Options.UseFont = true;
+            this.roundedButton1.cBack = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.roundedButton1.cBackActive = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.roundedButton1.cBackHover = System.Drawing.Color.LightBlue;
+            this.roundedButton1.cBackHoverActive = System.Drawing.Color.LightBlue;
+            this.roundedButton1.cornerRadius = 20;
+            this.roundedButton1.cTitle = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.roundedButton1.cTitleActive = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.roundedButton1.cTitleActiveHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.roundedButton1.cTitleHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.roundedButton1.hasBoarder = false;
+            this.roundedButton1.imageClicked = null;
+            this.roundedButton1.imageFitType = 0;
+            this.roundedButton1.imageHover = null;
+            this.roundedButton1.imageNormal = null;
+            this.roundedButton1.isClicked = false;
+            this.roundedButton1.isHover = false;
+            this.roundedButton1.Location = new System.Drawing.Point(112, 450);
+            this.roundedButton1.Name = "roundedButton1";
+            this.roundedButton1.showBackground = false;
+            this.roundedButton1.Size = new System.Drawing.Size(153, 75);
+            this.roundedButton1.TabIndex = 9;
+            this.roundedButton1.Text = "simpleButton2";
+            this.roundedButton1.Title = "Count";
+            this.roundedButton1.titleOffset = new System.Drawing.Point(0, 0);
+            // 
+            // rbutton3
+            // 
+            this.rbutton3.cBack = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.rbutton3.cBackActive = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.rbutton3.cBackHover = System.Drawing.Color.LightBlue;
+            this.rbutton3.cBackHoverActive = System.Drawing.Color.LightBlue;
+            this.rbutton3.cornerRadius = 20;
+            this.rbutton3.cTitle = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbutton3.cTitleActive = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbutton3.cTitleActiveHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbutton3.cTitleHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbutton3.hasBoarder = false;
+            this.rbutton3.imageClicked = null;
+            this.rbutton3.imageFitType = 0;
+            this.rbutton3.imageHover = null;
+            this.rbutton3.imageNormal = null;
+            this.rbutton3.isClicked = false;
+            this.rbutton3.isHover = false;
+            this.rbutton3.Location = new System.Drawing.Point(112, 174);
+            this.rbutton3.Name = "rbutton3";
+            this.rbutton3.showBackground = false;
+            this.rbutton3.Size = new System.Drawing.Size(153, 75);
+            this.rbutton3.TabIndex = 4;
+            this.rbutton3.Text = "simpleButton2";
+            this.rbutton3.Title = "Count";
+            this.rbutton3.titleOffset = new System.Drawing.Point(0, 0);
+            // 
+            // rbutton2
+            // 
+            this.rbutton2.cBack = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.rbutton2.cBackActive = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.rbutton2.cBackHover = System.Drawing.Color.LightBlue;
+            this.rbutton2.cBackHoverActive = System.Drawing.Color.LightBlue;
+            this.rbutton2.cornerRadius = 20;
+            this.rbutton2.cTitle = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbutton2.cTitleActive = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbutton2.cTitleActiveHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbutton2.cTitleHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbutton2.hasBoarder = false;
+            this.rbutton2.imageClicked = null;
+            this.rbutton2.imageFitType = 0;
+            this.rbutton2.imageHover = null;
+            this.rbutton2.imageNormal = null;
+            this.rbutton2.isClicked = false;
+            this.rbutton2.isHover = false;
+            this.rbutton2.Location = new System.Drawing.Point(112, 93);
+            this.rbutton2.Name = "rbutton2";
+            this.rbutton2.showBackground = false;
+            this.rbutton2.Size = new System.Drawing.Size(153, 75);
+            this.rbutton2.TabIndex = 3;
+            this.rbutton2.Text = "simpleButton1";
+            this.rbutton2.Title = "Count";
+            this.rbutton2.titleOffset = new System.Drawing.Point(0, 0);
+            // 
+            // rbutton1
+            // 
+            this.rbutton1.cBack = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.rbutton1.cBackActive = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.rbutton1.cBackHover = System.Drawing.Color.LightBlue;
+            this.rbutton1.cBackHoverActive = System.Drawing.Color.LightBlue;
+            this.rbutton1.cornerRadius = 20;
+            this.rbutton1.cTitle = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbutton1.cTitleActive = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbutton1.cTitleActiveHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbutton1.cTitleHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rbutton1.hasBoarder = false;
+            this.rbutton1.imageClicked = null;
+            this.rbutton1.imageFitType = 0;
+            this.rbutton1.imageHover = null;
+            this.rbutton1.imageNormal = null;
+            this.rbutton1.isClicked = false;
+            this.rbutton1.isHover = false;
+            this.rbutton1.Location = new System.Drawing.Point(112, 12);
+            this.rbutton1.Name = "rbutton1";
+            this.rbutton1.showBackground = false;
+            this.rbutton1.Size = new System.Drawing.Size(153, 75);
+            this.rbutton1.TabIndex = 0;
+            this.rbutton1.Text = "simpleButton1";
+            this.rbutton1.Title = "Count";
+            this.rbutton1.titleOffset = new System.Drawing.Point(0, 0);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 586);
+            this.Controls.Add(this.roundedButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rbutton3);
+            this.Controls.Add(this.rbutton2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rbutton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private RoundedButton rbutton1;
+        
+        private System.Windows.Forms.Button button2;
+        private RoundedButton rbutton2;
+        private RoundedButton rbutton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private RoundedButton roundedButton1;
+    }
+}
+
